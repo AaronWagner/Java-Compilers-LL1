c7123ad8a9643c96e48ef4a91a8d497428153b38
AaronWagner <aaron.p.wagner@gmail.com>
2015-02-19 16:35:11 -0500

    untested writing the recursion rules in progress

--- src/Main.java
+++ src/Main.java
@@ -8,51 +8,11 @@ public class Main
 	// write your code here
     }
 
-   class Rule
-   {
-       String leftHandSide;
-       ArrayList<String> rightHandSide;
-
-       public  Rule(String inputLeftSide, ArrayList<String> inputRightSide)
-       {
-           leftHandSide=new String(inputLeftSide);
-           for (int i=0; i<inputRightSide.size();i++  )
-           {
-               rightHandSide.add(inputRightSide.get(i));
-           }
-       }
 
 
-   }
 
-    class Language
-    {
-        ArrayList<Rule> myLanguage;
 
-        public Language()
-        {}
 
-        public void addRule()
-        {
 
-        }
-
-        public void removeRecursion()
-        {
-            int i=0;
-            int j=0;
-            while (i<myLanguage.size())
-            {
-                while (j<myLanguage.size())
-                {
-
-                    j++;
-                }
-
-                i++;
-            }
-
-        }
-    }
 
 }
---
+++ src/Output.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Aaron on 2/19/2015.
+ */
+public class Output {
+
+        ArrayList<String> characters;
+
+}
